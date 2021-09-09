5283441ede8bc16545a7a49723f5f06722b05786
IsaacMorrier <isaacmorrier@gmail.com>
2021-09-09 17:57:09 -0400

    add wide gamut red and css tweaks

--- web/src/routes/info.svelte
+++ web/src/routes/info.svelte
@@ -25,34 +25,40 @@ import SanityClientConstructor from "@sanity/client";
 			--quinary-color: #{ get-color(egg)};
 			--quinary-color-hover: #{ mix(get-color(egg), get-color(red), 80%)};
 		}
+		footer div {
+			opacity: 0.3;
+		}
 	</style>
   </svelte:head>
 
 <article>
 	<h1>Hi,<br> I’m Isaac Morrier</h1>
 
-	<p class="lede">I study branding, products, people, material culture, and the effects technology has on all of these things and help companies create competitive advantage with that insight.</p>
-	
-	<p>Lately I wrote the strategy for an international soda redesign, launched an energy bar you could order via text message, tweeted llama gifs for a $2 trillion bank, found a new identity for America’s oldest performance running shoe brand, positioned zero-amenity micro-apartments, found a universal language for recycling, growth-hacked coffee, and helped launch the unofficial memecoin of Hustle Twitter.</p>
-	
-	<p>These are some of the things I care about:</p>
-	<ul>
-		<li>people</li>
-		<li>the future</li>
-		<li>typography</li>
-	</ul>
-	
-	<p>These are some of the things I don’t care about:</p>
-	
-	<ul><li>Any World Series outcome after 2004</li></ul>
-	
-	<p>I currently work as a strategist at Design Army in Washington, DC and previously at Verb Energy, DigitasLBi, Trinity Mobile Networks, SapientNitro, and the Behavioral Research Lab at the Yale School of Management. I graduated from Yale University with my B.A. in Sociology in 2017.</p>
+	<div class="page-content">
 
-	<p>I built this website with Sapper, Sanity, and Netlify.</p>
-	
-	<p>Humans who want to work with me can contact me at isaac@isaacmorrier.com.</p>
-	
-	<p>Cheers,</p>
-	
-	<p>Isaac</p>
+		<p class="lede">I study branding, products, people, material culture, and the effects technology has on all of these things and help companies create competitive advantage with that insight.</p>
+		
+		<p>Lately I wrote the strategy for an international soda redesign, launched an energy bar you could order via text message, tweeted llama gifs for a $2 trillion bank, found a new identity for America’s oldest performance running shoe brand, positioned zero-amenity micro-apartments, found a universal language for recycling, growth-hacked coffee, and helped launch the unofficial memecoin of Hustle Twitter.</p>
+		
+		<p>These are some of the things I care about:</p>
+		<ul>
+			<li>people</li>
+			<li>the future</li>
+			<li>typography</li>
+		</ul>
+		
+		<p>These are some of the things I don’t care about:</p>
+		
+		<ul><li>Any World Series outcome after 2004</li></ul>
+		
+		<p>I currently work as a strategist at Design Army in Washington, DC and previously at Verb Energy, DigitasLBi, Trinity Mobile Networks, SapientNitro, and the Behavioral Research Lab at the Yale School of Management. I graduated from Yale University with my B.A. in Sociology in 2017.</p>
+
+		<p>I built this website with Sapper, Sanity, and Netlify.</p>
+		
+		<p>Humans who want to work with me can contact me at isaac@isaacmorrier.com.</p>
+		
+		<p>Cheers,</p>
+		
+		<p>Isaac</p>
+	</div>
 </article>
